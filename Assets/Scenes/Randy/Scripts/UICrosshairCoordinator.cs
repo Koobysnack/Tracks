10e13ef06056cf522c45a87226134f410fd2a791
TeiHek <lerandy1235@gmail.com>
2023-04-12 20:25:11 -0700

    Revolver scrolling improvements

--- Assets/Scenes/Randy/Scripts/UICrosshairCoordinator.cs
+++ Assets/Scenes/Randy/Scripts/UICrosshairCoordinator.cs
@@ -10,11 +10,13 @@ public class UICrosshairCoordinator : MonoBehaviour
     private List<Image> bulletDisplay = new List<Image>();
     [SerializeField] private GameObject uiPivot;
     [SerializeField] private Image imagePrefab;
+    Quaternion endAngle;
+    bool rotating;
+    List<Quaternion> angles = new List<Quaternion>();
 
     private void Awake()
     {
-        
-        
+        rotating = false;    
     }
 
     // Start is called before the first frame update
@@ -39,18 +41,62 @@ public class UICrosshairCoordinator : MonoBehaviour
             newBullet.rectTransform.anchoredPosition = new Vector2(UICMan.radius * Mathf.Sin(360f / 7f * i * Mathf.Deg2Rad), UICMan.radius * Mathf.Cos(360f / 7f * i * Mathf.Deg2Rad));
             bulletDisplay.Add(newBullet);
         }
+        uiPivot.GetComponentInChildren<Image>().color = Color.blue;
     }
 
     public IEnumerator RotateChamber(float target)
     {
+        angles.Add(Quaternion.Euler(0, 0, target));
+        if (rotating)
+        {
+            yield break;
+        }
+        rotating = true;
         float time = 0;
         Quaternion startAngle = uiPivot.transform.rotation;
-        Quaternion endAngle = Quaternion.Euler(0, 0, target);
+        int i = 0;
         while (time < UICMan.duration)
         {
-            uiPivot.transform.rotation = Quaternion.Lerp(startAngle, endAngle, time/UICMan.duration);
+            uiPivot.transform.rotation = Quaternion.Lerp(startAngle, angles[i], time/(UICMan.duration * (i + 1) / angles.Count) );
+            time += Time.deltaTime;
+            if(time >= UICMan.duration * (i + 1) / angles.Count)
+            {
+                startAngle = uiPivot.transform.rotation;
+                i++;
+            }
+            yield return null;
+        }
+        // Finalize rotation so it doesn't end at a weird spot
+        uiPivot.transform.rotation = angles[angles.Count - 1];
+        angles.Clear();
+        rotating = false;
+    }
+
+    public IEnumerator RotateChambers(float target)
+    {
+        Quaternion startAngle = uiPivot.transform.rotation;
+        endAngle = Quaternion.Euler(0, 0, target);
+        if (rotating)
+        {
+            yield break;
+        }
+        rotating = true;
+        float time = 0;
+        while (time < UICMan.duration)
+        {
+            uiPivot.transform.rotation = Quaternion.Lerp(startAngle, endAngle, time / UICMan.duration);
             time += Time.deltaTime;
             yield return null;
         }
+        // Finalize rotation so it doesn't end at a weird spot
+        uiPivot.transform.rotation = Quaternion.Euler(0, 0, target);
+        rotating = false;
     }
+
+    //public IEnumerator RotateTest(float target)
+    //{
+    //    float speed = 2f;
+    //    Vector3 targetDirection = new Vector3(Mathf.Cos(target), Mathf.Sin(target));
+    //    while (uiPivot.transform.rotation != targetDirection)
+    //}
 }
