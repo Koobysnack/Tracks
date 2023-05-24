aa144b8ea067b2803b8fe380f120f49bdfebe95d
TeiHek <lerandy1235@gmail.com>
2023-05-24 00:24:20 -0700

    Added optoions menu to in game pause

---
+++ Assets/Scenes/Randy/Scripts/NonUIScripts/SinSwitchInteractable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinSwitchInteractable : PickUp
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void Interaction()
+    {
+        // Open Menu
+    }
+}
