71b0307794fa606ec0c6e095d63ab5aa77f9119f
TeiHek <lerandy1235@gmail.com>
2023-05-25 15:39:06 -0700

    Reformatted EnvySin, added sfx

--- Assets/Scenes/Daniel/Scripts/Attacks/AbsSinClass.cs
+++ Assets/Scenes/Daniel/Scripts/Attacks/AbsSinClass.cs
@@ -9,9 +9,9 @@ public abstract class  AbsSinClass : MonoBehaviour
 {
 
     // Start is called before the first frame update
-    [SerializeField] float cooldown;
-    [SerializeField] float damage;
-    [SerializeField] string sfx;
+    [SerializeField] protected float cooldown;
+    [SerializeField] protected float damage;
+    [SerializeField] protected string sfx;
 
     public bool showDebugInfo;
     [ShowIf("showDebugInfo")] public bool ready;
@@ -35,5 +35,17 @@ public abstract class  AbsSinClass : MonoBehaviour
             yield return null;
         }
         ready = true;
+    }
+
+    protected void SinFireSFX()
+    {
+        if (string.IsNullOrEmpty(sfx))
+            return;
+        var sound = FMODUnity.RuntimeManager.CreateInstance(sfx);
+        sound.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        if (MusicManager.instance != null)
+            MusicManager.instance.TriggerSFX(sound, 8);
+        //sound.start();
+        //sound.release();
     }
 }
--- Assets/Scenes/Daniel/Scripts/Attacks/EnvySin.cs
+++ Assets/Scenes/Daniel/Scripts/Attacks/EnvySin.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnvySin : AbsSinClass 
+public class EnvySin : AbsSinClass
 {
 
     [SerializeField]
@@ -15,91 +15,86 @@ public class EnvySin : AbsSinClass
         lMask = LayerMask.GetMask("Enemy,Ground");
     }
 
-    
-    public override void SinFire(Transform shotOrigin )
+
+    public override void SinFire(Transform shotOrigin)
     {
         StartCoroutine(StartCooldown());
         RaycastHit hit;
         lMask = LayerMask.GetMask("Enemy,Ground");
-
+        SinFireSFX();
 
         // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(shotOrigin.position, shotOrigin.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
         {
-            Debug.DrawRay(shotOrigin.position, shotOrigin.TransformDirection(Vector3.forward) * hit.distance, Color.red,1000);
+            Debug.DrawRay(shotOrigin.position, shotOrigin.TransformDirection(Vector3.forward) * hit.distance, Color.red, 1000);
             Debug.Log("Did Hit");
 
             //  RaycastHit pierceHit;
 
             // print(hit);
-           
-                EnvyPierce(hit, shotOrigin.TransformDirection(Vector3.forward),shotOrigin);
-            
+
+            EnvyPierce(hit, shotOrigin.TransformDirection(Vector3.forward), shotOrigin);
+
         }
         else
         {
-            Debug.DrawRay(shotOrigin.position,shotOrigin.TransformDirection(Vector3.forward) * 1000, Color.white,1000);
+            Debug.DrawRay(shotOrigin.position, shotOrigin.TransformDirection(Vector3.forward) * 1000, Color.white, 1000);
             Debug.Log("Did not Hit");
         }
 
     }
     //recursively call this function each time a pierce occurs
-    private void EnvyPierce(RaycastHit obj, Vector3 fireAngle,Transform shotOrigin)
+    private void EnvyPierce(RaycastHit obj, Vector3 fireAngle, Transform shotOrigin)
     {
-     //   RaycastHit hit;
+        //   RaycastHit hit;
         RaycastHit backHit;
         RaycastHit nextObj;
 
-
         Debug.Log(obj.point);
         Ray backRay = new Ray(obj.point + fireAngle * 6, -1 * fireAngle * 6);
-      
-            obj.collider.Raycast(backRay, out backHit, 1000);
-       // Debug.Log("stinky");
+
+        obj.collider.Raycast(backRay, out backHit, 1000);
+        // Debug.Log("stinky");
         Debug.Log(backHit.collider);
         Debug.Log(backHit.point);
-      
-            if (backHit.collider == null ||backHit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
-            {
 
-                return;
-            }
-        
-        
-
-        if (backHit.collider!=null && backHit.transform.parent)
+        if (backHit.collider == null || backHit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
         {
-           // Debug.Log(backHit.transform.gameObject.name);
+            return;
+        }
+
+        if (backHit.collider != null && backHit.transform.parent)
+        {
+            // Debug.Log(backHit.transform.gameObject.name);
             EntityController ShotEntity;
             ShotEntity = backHit.transform.parent.GetComponent<EntityController>();
             if (ShotEntity)
             {
-                ShotEntity.Damage(20, shotOrigin);
+                ShotEntity.Damage(damage, shotOrigin);
 
             }
-          
-
         }
         //then get next  object, 
-      
-       // Debug.DrawRay(backHit.point, fireAngle, Color.green, 10000);
-        if (Physics.Raycast(backHit.point, fireAngle, out nextObj, 1000)&& backHit.point != obj.point) {
-          //  Debug.Log("stinky");
-            if (nextObj.collider != null )
+
+        // Debug.DrawRay(backHit.point, fireAngle, Color.green, 10000);
+        if (Physics.Raycast(backHit.point, fireAngle, out nextObj, 1000) && backHit.point != obj.point)
+        {
+            //  Debug.Log("stinky");
+            if (nextObj.collider != null)
             {
                 Debug.DrawRay(backHit.point, fireAngle, Color.green, 10000);
 
                 EnvyPierce(nextObj, fireAngle, shotOrigin);
             }
-            }
-            else
-            {
-                return;
-            }
+        }
+        else
+        {
+            return;
+        }
 
-            //pass obj into envy again
+        //pass obj into envy again
 
-        
-        
-    } 
+
+
+    }
 }
